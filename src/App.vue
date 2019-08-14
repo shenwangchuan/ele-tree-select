61bcf19218a6b31ee082890088ddd03011258342
shenwangchuan <yisu512@sina.com>
2019-08-14 12:04:37 +0800

    init

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,52 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <el-form :model="form" style="width: 500px">
+      <el-form-item label="单选：">
+        <TreeSelect v-model="form.radioV" :options="allArea" :props="areaProps"></TreeSelect>
+      </el-form-item>
+      <el-form-item label="多选：">
+        <TreeSelect v-model="form.multiV" :options="allArea" :props="areaProps" multiple></TreeSelect>
+      </el-form-item>
+      <el-form-item>
+        <el-button @click="onSubmit">提交</el-button>
+      </el-form-item>
+    </el-form>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+  import TreeSelect from './components/TreeSelect'
 export default {
   name: 'app',
-  components: {
-    HelloWorld
+  components: {TreeSelect},
+  data() {
+    return {
+      form: {
+        radioV:'',
+        multiV: [2]
+      },
+      allArea: [],
+      areaProps: {
+        label: 'area_name',
+        value: 'area_code',
+        children: 'sub'
+      }
+    }
+  },
+  created() {
+    this.getJsonData()
+  },
+  methods: {
+    getJsonData() {
+      this.$axios.get('json/area.json').then(r=>{
+        if (r.status === 200 &&  r.data.content) {
+          this.allArea = r.data.content
+        }
+      })
+    },
+    onSubmit() {
+      console.log(this.form)
+    }
   }
 }
 </script>
@@ -23,6 +58,6 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  padding: 20px;
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,7 +1,12 @@
 import Vue from 'vue'
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
 import App from './App.vue'
+import axios from 'axios'
 
 Vue.config.productionTip = false
+Vue.use(ElementUI)
+Vue.prototype.$axios=axios
 
 new Vue({
   render: h => h(App),
